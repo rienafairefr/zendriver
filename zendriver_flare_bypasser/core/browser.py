027a311c16db4ec53cbfb9386ac6df8262c901a7
Matthieu Berthomé <cheery.iron6016@fastmail.com>
2025-05-29 23:27:59 +0200

    logs

--- zendriver_flare_bypasser/core/browser.py
+++ zendriver_flare_bypasser/core/browser.py
@@ -337,6 +337,7 @@ class Browser:
         logger.info(
             "starting\n\texecutable :%s\n\narguments:\n%s", exe, "\n\t".join(params)
         )
+        logger.info(f"CMD: {shlex.join(exec, *params)}")
         if not connect_existing:
             self._process: asyncio.subprocess.Process = (
                 await asyncio.create_subprocess_exec(
