8d908b53e81a30675e4fb38a1637ecdcb7a9375f
Matthieu Berthomé <cheery.iron6016@fastmail.com>
2025-05-30 17:15:14 +0200

    sleep

--- zendriver_flare_bypasser/core/browser.py
+++ zendriver_flare_bypasser/core/browser.py
@@ -358,13 +358,13 @@ class Browser:
         self._http = HTTPApi((self.config.host, self.config.port))
         util.get_registered_instances().add(self)
         await asyncio.sleep(0.25)
-        for _ in range(5):
+        for _ in range(50):
             try:
                 self.info = ContraDict(await self._http.get("version"), silent=True)
             except (Exception,):
-                if _ == 4:
+                if _ == 49:
                     logger.debug("could not start", exc_info=True)
-                await self.sleep(0.5)
+                await asyncio.sleep(0.5)
             else:
                 break
 
